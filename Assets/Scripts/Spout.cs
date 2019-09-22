66c8a7f90b67694b2c934a4702c53f4a1f895ce6
Vast <Vastlee@gmail.com>
2019-09-22 12:31:27 -0700

    Added Project from Demonstration

---
+++ Assets/Scripts/Spout.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class Spout : MonoBehaviour {
+    [SerializeField]
+    private KeyCode sprayKey = KeyCode.Space;
+    [SerializeField]
+    private GameObject dropPrefab;
+    [SerializeField]
+    private Transform spawnPoint;
+    [SerializeField]
+    private float spoutPower = 5.0f;
+    [SerializeField]
+    private float dirRandomness = 5.0f;
+
+    private void Update() {
+        if (Input.GetKey(sprayKey)) {
+            Spray();
+        }
+    }
+
+    private void Spray() {
+        GameObject go = Instantiate(dropPrefab, spawnPoint.position, Quaternion.identity);
+
+        go.GetComponent<Renderer>().material.SetColor("_Color", RandColor());
+        go.GetComponent<Renderer>().material.SetColor("_EmissionColor", RandColor());
+
+        go.GetComponent<Rigidbody>().velocity = (transform.up * spoutPower) + RandDirection();
+    }
+
+    private Vector3 RandDirection() {
+        return new Vector3(
+            UnityEngine.Random.Range(-dirRandomness, dirRandomness),
+            0,
+            UnityEngine.Random.Range(-dirRandomness, dirRandomness)
+        );
+    }
+
+    private Color RandColor() {
+        return new Color(
+            UnityEngine.Random.value,
+            UnityEngine.Random.value,
+            UnityEngine.Random.value,
+            UnityEngine.Random.value
+        );
+    }
+}
