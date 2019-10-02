4e0779d22cb238c95fc1ba23184ee581e5803cde
Vast <Vastlee@gmail.com>
2019-10-01 19:57:05 -0700

    Added ShowChildCount script.

---
+++ Assets/Scripts/ShowChildCount.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ShowChildCount : MonoBehaviour {
+    [SerializeField]
+    private bool showChildCount = true;
+
+    private string originalName;
+    private int lastCount = -1;
+
+    void Start() {
+        originalName = gameObject.name;
+    }
+
+    void Update() {
+        if (showChildCount && (transform.childCount != lastCount)) {
+            gameObject.name = $"{originalName} ({transform.childCount})";
+            lastCount = transform.childCount;
+        }
+    }
+}
